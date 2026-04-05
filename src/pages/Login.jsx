7dd1a0e815c1d788417e71980d09b983eb0dbe8e
yousung <doyousung@naver.com>
2026-04-05 15:57:20 +0900

    feat : 로그인 페이지 UI 구성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
+import styles from "./Login.module.css";
+import powerIcon from "../assets/icon/Power.svg";
+import kakaoBtnImg from "../assets/icon/kakaologin.png";
+
 export default function Login() {
   return (
     <>
-      <h1>로그인</h1>
+      <div className={styles.login}>
+        <h1 className={styles.title}>
+          <span className={styles.loginTitleText}>SWITCH</span>
+          <img src={powerIcon} alt="Power Icon" className={styles.powerIcon} />
+          <span className={styles.loginTitleText}>N</span>
+        </h1>
+        <p className={styles.loginSubtitle}>
+          소셜 계정으로 간편하게 가입하기
+        </p>
+        <button className={styles.loginButton} type="button"><img src={kakaoBtnImg} alt="카카오톡으로 로그인"/></button>
+      </div>
     </>
   );
 }
